477a5a209d6a572adb1468a98587f42993a5cdde
AKzPrisma <ahmadkurniaprisma@gmail.com>
2026-07-06 20:05:17 +0700

    chore: add foto landing

--- frontend/src/data/homeExtraData.jsx
+++ frontend/src/data/homeExtraData.jsx
@@ -1,16 +1,16 @@
 export const floatingIcons = [
 
-  { id: 1, src: "", alt: "Genshin", className: "top-[5%] left-[-4%] w-[180px] h-[180px] md:w-[240px] md:h-[240px] rounded-[2.5rem] -translate-x-[20%]" },
-  { id: 2, src: "", alt: "Roblox", className: "top-[43%] left-[11%] w-[60px] h-[60px] md:w-[80px] md:h-[80px] rounded-[1.2rem] " },
-  { id: 3, src: "", alt: "MLBB", className: "top-[53%] left-[-3%] w-[130px] h-[130px] md:w-[150px] md:h-[150px] rounded-[2rem] -translate-x-[30%]" },
-  { id: 4, src: "", alt: "Neko Kiri", className: "bottom-[7%] left-[7%] w-[90px] h-[90px] md:w-[110px] md:h-[110px] rounded-[1.5rem]" },
-  { id: 5, src: "", alt: "Among Us", className: "bottom-[-5%] left-[-4%] w-[100px] h-[100px] md:w-[120px] md:h-[120px] rounded-[2rem] -translate-x-[30%]" },
+  { id: 1, src: "https://i.pinimg.com/736x/ab/e0/19/abe019d59a86a88b52a6257ea454269e.jpg", alt: "Genshin", className: "top-[5%] left-[-4%] w-[180px] h-[180px] md:w-[240px] md:h-[240px] rounded-[2.5rem] -translate-x-[20%]" },
+  { id: 2, src: "https://i.pinimg.com/736x/c0/34/14/c03414d395a7b0a733d025e4169814d3.jpg", alt: "Roblox", className: "top-[43%] left-[11%] w-[60px] h-[60px] md:w-[80px] md:h-[80px] rounded-[1.2rem] " },
+  { id: 3, src: "https://i.pinimg.com/736x/82/55/03/8255033248d018b6c5f3d460b2deec16.jpg", alt: "MLBB", className: "top-[53%] left-[-3%] w-[130px] h-[130px] md:w-[150px] md:h-[150px] rounded-[2rem] -translate-x-[30%]" },
+  { id: 4, src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Sw98BYxBbWSZhOu3xpUngiX2DubrL_87tOUZXB5wq4DeQGPi25fkzUU&s=10", alt: "Neko Michi", className: "bottom-[7%] left-[7%] w-[90px] h-[90px] md:w-[110px] md:h-[110px] rounded-[1.5rem]" },
+  { id: 5, src: "https://static.wikia.nocookie.net/among-us-wiki/images/8/84/Among_Us.png/revision/latest?cb=20240408020746", alt: "Among Us", className: "bottom-[-5%] left-[-4%] w-[100px] h-[100px] md:w-[120px] md:h-[120px] rounded-[2rem] -translate-x-[30%]" },
   
-  { id: 6, src: "", alt: "Outlast", className: "top-[2%] right-[-2%] w-[120px] h-[120px] md:w-[150px] md:h-[150px] rounded-[2rem] translate-x-[20%]" },
-  { id: 7, src: "", alt: "PUBG", className: "top-[20%] right-[13%] w-[70px] h-[70px] md:w-[85px] md:h-[85px] rounded-2xl" },
-  { id: 8, src: "", alt: "CoC", className: "top-[32%] right-[0%] w-[220px] h-[220px] md:w-[270px] md:h-[270px] rounded-[3.5rem] translate-x-[35%]" },
-  { id: 9, src: "", alt: "Neko Kanan", className: "bottom-[7%] right-[12%] w-[70px] h-[70px] md:w-[85px] md:h-[85px] rounded-[1.2rem]" },
-  { id: 10, src: "", alt: "Resident Evil", className: "bottom-[-13%] right-[0%] w-[140px] h-[140px] md:w-[170px] md:h-[170px] rounded-[2rem] translate-x-[20%]" },
+  { id: 6, src: "https://m.media-amazon.com/images/I/51RgpLOJkJL._AC_UF894,1000_QL80_.jpg", alt: "Outlast", className: "top-[2%] right-[-2%] w-[120px] h-[120px] md:w-[150px] md:h-[150px] rounded-[2rem] translate-x-[20%]" },
+  { id: 7, src: "https://egoamo.co.za/cdn/shop/products/FP4692-PUBG-playerunkown.jpg?v=1590118908", alt: "PUBG", className: "top-[20%] right-[13%] w-[70px] h-[70px] md:w-[85px] md:h-[85px] rounded-2xl" },
+  { id: 8, src: "https://i.pinimg.com/736x/70/c6/aa/70c6aac53e075c1512077a142d883f76.jpg", alt: "CoC", className: "top-[32%] right-[0%] w-[220px] h-[220px] md:w-[270px] md:h-[270px] rounded-[3.5rem] translate-x-[35%]" },
+  { id: 9, src: "https://i.pinimg.com/736x/34/15/57/34155708d1aada8d9dc0f6ce501db56f.jpg", alt: "Stumble Guys", className: "bottom-[7%] right-[12%] w-[70px] h-[70px] md:w-[85px] md:h-[85px] rounded-[1.2rem]" },
+  { id: 10, src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIaQKhbnSnntIoHTgBj_Tg9utlBxZdvZEUamxpF8UQAg&s=10", alt: "Resident Evil", className: "bottom-[-13%] right-[0%] w-[140px] h-[140px] md:w-[170px] md:h-[170px] rounded-[2rem] translate-x-[20%]" },
 ];
 
 export const popularGamesData = [
@@ -19,27 +19,27 @@ export const popularGamesData = [
     desc: "Petualangan fantasi di dunia terbuka.",
     rating: "18+",
     badgeColor: "bg-red-600 shadow-[0_4px_12px_rgba(220,38,38,0.3)]",
-    img: ""
+    img: "https://i.pinimg.com/736x/ab/e0/19/abe019d59a86a88b52a6257ea454269e.jpg"
   },
   {
     title: "Mobile Legends",
     desc: "MOBA strategi dengan pertarungan tim.",
     rating: "18+",
     badgeColor: "bg-red-600 shadow-[0_4px_12px_rgba(220,38,38,0.3)]",
-    img: ""
+    img: "https://i.pinimg.com/736x/82/55/03/8255033248d018b6c5f3d460b2deec16.jpg"
   },
   {
     title: "Resident Evil",
     desc: "Horor bertahan hidup melawan zombie.",
     rating: "18+",
     badgeColor: "bg-red-600 shadow-[0_4px_12px_rgba(220,38,38,0.3)]",
-    img: ""
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTIaQKhbnSnntIoHTgBj_Tg9utlBxZdvZEUamxpF8UQAg&s=10"
   },
   {
     title: "Neko Michi",
     desc: "Petualangan ringan dengan karakter kucing.",
     rating: "3+",
     badgeColor: "bg-blue-500 shadow-[0_4px_12px_rgba(59,130,246,0.3)]",
-    img: ""
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3Sw98BYxBbWSZhOu3xpUngiX2DubrL_87tOUZXB5wq4DeQGPi25fkzUU&s=10"
   }
 ];
